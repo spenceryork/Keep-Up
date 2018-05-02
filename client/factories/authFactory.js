413b35fb4d8e9482df1b9a759708ba0808218a81
Spencer York <spencerwyork@gmail.com>
2018-05-02 15:29:27 -0500

    added code that will keep user logged in on page refresh

--- client/factories/authFactory.js
+++ client/factories/authFactory.js
@@ -1,27 +1,60 @@
 "use strict";
 
-angular.module("KeepUp").factory("AuthFactory", ($http) => {
+angular.module("KeepUp").factory("AuthFactory", ($q, $http, $rootScope) => {
 
     let currentUser = null;
 
     return {
         createUser(userObj) {
-            return $http.post("/register", userObj).then(userData => {
-                // console.log("new user added", userData.data);
-                currentUser = userData;
-                return userData.data;
+            return $q((resolve, reject) => {
+                $http.post("/register", userObj).then(userData => {
+                    console.log("new user added", userData);
+                    currentUser = userData;
+                    resolve(userData.data);
+                });
+            }).catch(err => {
+                reject(err);
             });
         },
 
         loginUser(userObj) {
-            return $http.post("/login", userObj).then(user => {
-                currentUser = user.data;
+            console.log("userObj", userObj);
+
+            return $q((resolve, reject) => {
+                $http.post("/login", userObj).then(user => {
+                    currentUser = user.data;
+                    resolve(user.data);
+                });
+            }).catch(err => {
+                reject(err);
             });
         },
 
         getCurrentUser() {
             return currentUser;
+        },
+
+        // This is called from app.js as a way of confirming whether or not we have a loged-in user. If so, we set currentUser to that value
+        setUserStatus() {
+            return $http
+                .get("/status")
+                .then(user => {
+                    console.log("user in set user status", user);
+
+                    if (user) {
+                        currentUser = user.data;
+                    } else {
+                        currentUser = null;
+                    }
+                })
+                .catch(() => {
+                    currentUser = null;
+                });
+        },
+
+        broadcastUserLogin(user) {
+            console.log("calling broadcast", user);
+            $rootScope.$broadcast("handleBroadcast", user);
         }
     };
-
-})
+});
